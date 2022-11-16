962ca1d3921f62a8387a7b066b2f37f72cac6fa9
JayPlayer369 <113185518+JayPlayer369@users.noreply.github.com>
2022-11-16 09:01:13 -0500

    Added health and objects

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-   Rigidbody2D rigidbody2D;
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+    public int health{ get { return currentHealth;}}
+
+    Rigidbody2D rigidbody2D;
    float horizontal;
    float vertical;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        
     }
+
+     
+
 
     // Update is called once per frame
     void Update()
@@ -31,5 +40,11 @@ public class RubyController : MonoBehaviour
         position.y = position.y + 5.0f * vertical * Time.deltaTime;
         
         rigidbody2D.MovePosition(position);
+    } 
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
+
 }
