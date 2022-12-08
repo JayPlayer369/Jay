f47c93f485db4b2f1ac5072b9ebed22852d5ea7c
JayPlayer369 <113185518+JayPlayer369@users.noreply.github.com>
2022-12-08 09:09:08 -0500

    Added Projectiles

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,7 +12,9 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
-     Animator animator;
+    Animator animator;
+
+    bool broken = true;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,12 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if(!broken)
+        {
+            return;
+        }
+ 
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -36,6 +44,12 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
+         if(!broken)
+        {
+             return;
+        }
+
+    
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
@@ -46,9 +60,9 @@ public class EnemyController : MonoBehaviour
         }
         else
         {
-        position.x = position.x + Time.deltaTime * speed * direction;
-                    animator.SetFloat("Move X", direction);
-                    animator.SetFloat("Move Y", 0); 
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0); 
         }
 
 
@@ -63,6 +77,13 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
     }
 }    
 
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -9,15 +9,25 @@ public class RubyController : MonoBehaviour
     public int maxHealth = 5;
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
     public int health{ get { return currentHealth;}}
 
     Rigidbody2D rigidbody2D;
    float horizontal;
    float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
     }
 
@@ -30,10 +40,34 @@ public class RubyController : MonoBehaviour
          horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
 
-        Debug.Log(horizontal);
-        
+         Vector2 move = new Vector2(horizontal, vertical);
+
+         if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+                
         
     }
+
     void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
@@ -42,10 +76,22 @@ public class RubyController : MonoBehaviour
         
         rigidbody2D.MovePosition(position);
     } 
+
     public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+    
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2D.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+
     }
 
 } 
